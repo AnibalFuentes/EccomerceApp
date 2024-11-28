c7273a2faf91c30155481fc5387a2502ad0cb87a
AnibalFuentes <anibaldfuentesprieto@gmail.com>
2024-11-28 02:39:27 -0500

    otra vee

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,11 +1,10 @@
+// app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+
 import prismadb from "@/lib/prismadb";
 import BillboardsForm from "./components/billboards-form";
 
-const BillboardPage = async ({
-  params,
-}: {
-  params: { billboardId: string };
-}) => {
+// Componente server-side asincrónico
+const BillboardPage = async ({ params }: { params: { billboardId: string } }) => {
   // Consulta asincrónica para obtener los datos del billboard
   const billboard = await prismadb.billboard.findUnique({
     where: {
@@ -13,6 +12,7 @@ const BillboardPage = async ({
     },
   });
 
+  // Renderizado del componente con datos cargados
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
