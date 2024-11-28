dfea053a3c1ac6237da55329bbb0eae9b83c6c0f
AnibalFuentes <anibaldfuentesprieto@gmail.com>
2024-11-28 02:52:53 -0500

    vamos de nuevo

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -4,29 +4,19 @@ import prismadb from "@/lib/prismadb";
 import BillboardsForm from "./components/billboards-form";
 
 // Asegúrate de que el tipo sea correcto para `params`
-interface BillboardPageProps {
-  params: {
-    billboardId: string;
-  };
-}
 
-const BillboardPage = async ({ params }: BillboardPageProps) => {
-  // Desestructuramos billboardId para un acceso más directo
-  const { billboardId } = params;
-
-  // Consulta asincrónica para obtener los datos del cartel
+export const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  // Consulta asincrónica para obtener los datos del billboard
   const billboard = await prismadb.billboard.findUnique({
     where: {
-      id: billboardId, // Usamos el id del parámetro
+      id: params.billboardId,
     },
   });
 
-  // Verifica si se encontró el billboard
-  if (!billboard) {
-    return <div>No se encontró el cartel.</div>;
-  }
-
-  // Renderizado del componente con datos cargados
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
@@ -35,5 +25,3 @@ const BillboardPage = async ({ params }: BillboardPageProps) => {
     </div>
   );
 };
-
-export default BillboardPage;
